513eadf8884edd4faad5ce497213bd1bc3db9c6d
Olajcodes <olajideabioye01@gmail.com>
2025-08-28 16:11:06 +0100

    trying to move

---
+++ week_four/task11/participant_pkg/file_ops.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+import csv
+from pathlib import Path
+
+contantspace = Path("workspace")
+contantspace.mkdir(exist_ok= True)
+csv_file = contantspace / "contacts.csv"
+
+
+#  Function to append participant details to a CSV file
+def save_participant(path, participant_dict):
